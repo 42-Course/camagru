c7fe6da9757b3a0c0f6969074101ef1fc777cae8
pulgamecanica <pulgamecanica11@gmail.com>
2025-07-25 16:22:52 +0200

    chore: add CORS

--- backend/app.rb
+++ backend/app.rb
@@ -1,17 +1,21 @@
 require "sinatra"
 require "sinatra/reloader" if development?
+require_relative "./app/lib/cors"
 
 # CamagruApp is the main Sinatra application class that handles
 # all HTTP routes and configuration for the Camagru backend.
 class CamagruApp < Sinatra::Base
   configure :production do
     puts "🛡️ Rack::Protection Middleware disabled"
+    use Rack::Protection::HostAuthorization, permitted_hosts: ["camagru42.fly.dev"]
   end
 
   configure :development do
     puts "♻️ Sinatra::Reloader enabled for development"
     register Sinatra::Reloader
   end
+
+  use CORS
 
   get "/" do
     {message: "Welcome to CamagruApp"}.to_json
